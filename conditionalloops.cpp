be9a494c608f0517fb481841b3d11b7492ded27f
Heyfronx <142912385+Heyfronx@users.noreply.github.com>
2024-08-14 19:11:08 +0530

    Add files via upload

---
+++ conditionalloops.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+using namespace std;
+//to print even and odd numbers
+int main()
+{
+    // int a;
+    // cout<<"enter a number: ";
+    // cin>>a;
+    // if(a%2==0)
+    // {
+    //     cout<<"even "<<a;
+    // }
+    // else{
+    //     cout<<"odd "<<a;
+    // }
+    //to find weather area of rectangle is greater than perimeter.
+    // int l,b;
+    // l=20; cout<<endl;
+    // b=30;
+    // int peri=2*(l+b);
+    // cout<<peri<<endl;
+    // int area=l*b;
+    // cout<<area<<endl;
+    // if(area>peri)
+    // {
+    //     cout<<"area of rectangle is greater ";
+    // }
+    // else{
+    //     cout<<"perimeter is greater "; 
+    // }
+    // int a,b,c; //to print largest number out of 3.
+    // cout<<"enter three numbers: ";
+    // cin>>a>>b>>c;
+    // cout<<endl;
+    // if(a>b&&a>c)
+    // {
+    //     cout<<"a is the greatest: "<<a;
+    // }
+    // else if (b>c&&b>a)
+    // {
+    //     cout<<"b is greatest: "<<b;
+    // }
+    // else if(c>a&&c>b)
+    // {
+    //     cout<<"c is greatest: "<<c;
+
+    // }
+    // else{
+    //     cout<<"wrong input";
+    // }
+    //ternary operator
+    // int a=50;
+    // (a%5==0)? cout<<"even":cout<<"odd";
+    //for loop
+    // for(int i=19;i<=190;i=i+19)
+    // {
+    //     cout<<i<<endl;
+    // }
+    
+}
